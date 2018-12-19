dcc20eeedc74c451335fa50997b68d1986981f1f
Shamar <kyudeci@gmail.com>
2018-12-19 16:56:26 -0500

    Log in and sign up

--- db/migrate/001_create_users.rb
+++ db/migrate/001_create_users.rb
@@ -1,8 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def up
     create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :birth_date
       t.string :username
       t.string :password
+      t.string :email
+
     end
   end
 
--- server.rb
+++ server.rb
@@ -1,13 +1,33 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+require 'sqlite3'
+require 'active_record'
+require './models/user.rb'
+
+enable :sessions
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: 'db/user.db'
+)
 # ruby server.rb -o 0.0.0.0 -p 3000
 get "/" do
   erb :index, :layout => :primary_layout
 end
 # Instead of creating a seperate page, create a modal?.
-get "/signup" do
-  erb :signup
+get "/blog" do
+  @user = User.find(session[:user_id])
+  erb :blogpost, :layout => :blog_layout
+end
+post "/" do
+  user = User.find_by(username: params["username"], password: params["password"])
+  if user
+    puts user
+    session[:user_id] = user.id
+    redirect '/blog'
+  else
+    redirect '/'
+  end
 end
 
 get "/login" do
