334366e5c645889ab1830c75ee7449a4ecfc08f4
Shamar <kyudeci@gmail.com>
2019-01-02 09:08:29 -0500

    Connection to Heroku

--- server.rb
+++ server.rb
@@ -7,10 +7,14 @@ require './models/user.rb'
 require './models/posts.rb'
 
 enable :sessions
-ActiveRecord::Base.establish_connection(
-  adapter: 'sqlite3',
-  database: 'db/user.db'
-)
+if ENV['DATABASE_URL']
+  ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+else
+  ActiveRecord::Base.establish_connection(
+    adapter: 'sqlite3',
+    database: 'db/user.db'
+  )
+end
 # ruby server.rb -o 0.0.0.0 -p 3000
 get "/" do
   erb :index, :layout => :primary_layout
@@ -34,13 +38,13 @@ end
 
 post "/signup" do
 copy_user = User.find_by(username: params["username"])
-    if copy_user
-        redirect "/"
-    else
-        user = User.create(first_name: params["first_name"], last_name: params["last_name"], birth_date: params["birth_date"], username: params["username"], password: params["password"], email: params["email"])
-        session[:user_id] = user.id
-        redirect '/'
-    end
+  if copy_user
+    redirect "/"
+  else
+    user = User.create(first_name: params["first_name"], last_name: params["last_name"], birth_date: params["birth_date"], username: params["username"], password: params["password"], email: params["email"])
+    session[:user_id] = user.id
+    redirect '/'
+  end
 end
 get "/logout" do
   session[:user_id] = nil
@@ -48,7 +52,7 @@ get "/logout" do
 end
 post "/blog" do
   @user = User.find(session[:user_id])
-  @userPost = Posts.create(user_id: session[:user_id], username: @user.username, user_post: params["user_post"])
+  @userPost = Posts.create(user_id: session[:user_id], username: @user.username, user_post: params["user_post"], title: params[:title])
   redirect "/blog"
 end
 get "/profile" do
