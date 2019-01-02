bb2c1e090e49d109436b51e7843816eb5a6b26c6
Shamar <kyudeci@gmail.com>
2019-01-02 09:45:55 -0500

    seeds.rb heroku integration

--- seeds.rb
+++ seeds.rb
@@ -7,7 +7,11 @@ require 'pry'
 
 # Connect to a sqlite3 database
 # If you feel like you need to reset it, simply delete the file sqlite makes
-ActiveRecord::Base.establish_connection(
-  adapter: 'sqlite3',
-  database: 'db/user.db'
-)
+if ENV['DATABASE_URL']
+  ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+else
+  ActiveRecord::Base.establish_connection(
+    adapter: 'sqlite3',
+    database: 'db/user.db'
+  )
+end
