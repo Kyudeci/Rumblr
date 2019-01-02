95dc138e0279aae2a7afc5fb8b0392ed03b7c37b
Shamar <kyudeci@gmail.com>
2019-01-02 10:01:58 -0500

    database fix

--- database.rb
+++ database.rb
@@ -1,5 +1,5 @@
 # Run this script with `bundle exec ruby app.rb`
-require 'sqlite3'
+require 'active_record'
 
 # Use `binding.pry` anywhere in this script for easy debugging
 require 'pry'
@@ -12,7 +12,7 @@ if ENV['DATABASE_URL']
   require 'pg'
   ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
 else
-  require 'active_record'
+  require 'sqlite3'
   ActiveRecord::Base.establish_connection(
     adapter: 'sqlite3',
     database: 'db/user.db'
